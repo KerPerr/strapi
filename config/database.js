6148bccd6752727912719b8123d6c4c77aaff7fa
Lenovo <p.castrec@gmail.com>
2022-03-15 15:26:18 +0100

    resolve

--- config/database.js
+++ config/database.js
@@ -6,8 +6,7 @@ module.exports = ({ env }) => ({
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'd6hcvaf9lg4re8'),
       user: env('DATABASE_USERNAME', 'ssqcdmglhqnewe'),
-      password: env('DATABASE_PASSWORD', 'f03ce2a5bf8a8e61cf1a3f43117a163060f852a482e5cf94d40d1f8632ea3108'),
-      ssl: env.bool('DATABASE_SSL', true),
+      password: env('DATABASE_PASSWORD', 'f03ce2a5bf8a8e61cf1a3f43117a163060f852a482e5cf94d40d1f8632ea3108')
     },
   },
 });
