07a1d4accc5baa547bddfa35af12a391497e1cb0
Lenovo <p.castrec@gmail.com>
2022-03-15 15:37:43 +0100

    update

--- config/database.js
+++ config/database.js
@@ -1,12 +1,18 @@
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'ec2-54-85-113-73.compute-1.amazonaws.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'd6hcvaf9lg4re8'),
-      user: env('DATABASE_USERNAME', 'ssqcdmglhqnewe'),
-      password: env('DATABASE_PASSWORD', 'f03ce2a5bf8a8e61cf1a3f43117a163060f852a482e5cf94d40d1f8632ea3108')
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
+      },
     },
+    debug: false,
   },
 });
